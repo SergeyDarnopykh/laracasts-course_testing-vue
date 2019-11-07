4a6c7df57b2bb1fd15a7e943c142c60fe2e1295b
sdarnopykh <sdarnopykh@oggettoweb.com>
2019-11-07 17:33:43 +0300

    Add lesson 7

---
+++ src/components/Question.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <div v-if="!editing">
+            <!-- Viewing the question-->
+            <h1>{{ question.title }}</h1>
+            <div class="body">
+                {{ question.body }}
+            </div>
+            <button id="edit" @click="editing = true">Edit</button>
+        </div>
+        <div v-if="editing">
+            <input type="text" name="title" v-model="form.title" />
+            <textarea name="body" v-model="form.body" />
+
+            <button id="cancel" @click="cancel">Cancel</button>
+            <button id="update" @click="update">Update</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['dataQuestion'],
+
+        data() {
+            return {
+                question: this.dataQuestion,
+                form: {
+                    title: this.dataQuestion.title,
+                    body: this.dataQuestion.body
+                },
+                editing: false
+            }
+        },
+
+        methods: {
+            cancel() {
+                this.editing = false
+            },
+
+            update() {
+                this.question.title = this.form.title
+                this.question.body = this.form.body
+
+                this.editing = false
+            }
+        }
+    }
+</script>
