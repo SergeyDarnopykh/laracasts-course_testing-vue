8722261baa3610d7bf9c39ce9cca62804a9c6438
sdarnopykh <sdarnopykh@oggettoweb.com>
2019-11-08 16:48:14 +0300

    Add lesson 10

--- test/countdown.spec.js
+++ test/countdown.spec.js
@@ -34,65 +34,53 @@ describe('Countdown', () => {
         see('10 Seconds')
     })
 
-    it ('reduces the countdown every second', (done) => {
+    it ('reduces the countdown every second', async () => {
         see('10 Seconds')
 
         clock.tick(time.SECOND)
 
-        assertOnNextTick(() => {
-            see('9 Seconds')
-        }, done)
+        await vm.$nextTick()
+
+        see('9 Seconds')
     })
 
-    it ('shows an expired message when the countdown has completed', (done) => {
+    it ('shows an expired message when the countdown has completed', async () => {
         clock.tick(10 * time.SECOND)
 
-        assertOnNextTick(() => {
-            see('Now expired')
-        }, done)
+        await vm.$nextTick()
+
+        see('Now expired')
     })
 
-    it ('shows a custom expired message when the countdown has completed', (done) => {
+    it ('shows a custom expired message when the countdown has completed', async () => {
         wrapper.setProps({ expiredText: 'Custom expired message'})
 
         clock.tick(10 * time.SECOND)
 
-        assertOnNextTick(() => {
-            see('Custom expired message')
-        }, done)
+        await vm.$nextTick()
+
+        see('Custom expired message')
     })
 
-    it ('broadcasts when the countdown is finished', (done) => {
+    it ('broadcasts when the countdown is finished', async () => {
         clock.tick(10 * time.SECOND)
 
-        assertOnNextTick(() => {
-            expectEvent('finished')
-        }, done)
+        await vm.$nextTick()
+
+        expectEvent('finished')
     })
 
-    it ('clears the interval while completed', (done) => {
+    it ('clears the interval while completed', async () => {
         clock.tick(10 * time.SECOND)
 
         expect(wrapper.vm.now.getSeconds()).toBe(10)
 
-        assertOnNextTick(() => {
-            clock.tick(5 * time.SECOND)
+        await vm.$nextTick()
 
-            expect(wrapper.vm.now.getSeconds()).toBe(10)
-        }, done)
+        clock.tick(5 * time.SECOND)
+
+        expect(wrapper.vm.now.getSeconds()).toBe(10)
     })
-
-    let assertOnNextTick = (cb, done) => {
-        vm.$nextTick(() => {
-            try {
-                cb()
-
-                done()
-            } catch(err) {
-                done(err)
-            }
-        })
-    }
 
     let expectEvent = (event) => {
         expect(wrapper.emitted()[event]).toBeTruthy()
